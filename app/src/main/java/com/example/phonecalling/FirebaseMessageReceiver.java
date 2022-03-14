0533029d37627beb8163cf5ebf77ad49bdc192c1
babyfattt <tokhanhxuan99@gmail.com>
2022-03-15 01:01:49 +0700

    add notification layout

--- app/src/main/java/com/example/phonecalling/FirebaseMessageReceiver.java
+++ app/src/main/java/com/example/phonecalling/FirebaseMessageReceiver.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
 import android.widget.RemoteViews;
 
 import androidx.core.app.NotificationCompat;
@@ -15,12 +16,14 @@ import com.google.firebase.messaging.RemoteMessage;
 
 public class FirebaseMessageReceiver extends FirebaseMessagingService {
 
-        // Override onMessageReceived() method to extract the
+    private static final String TAG = "MyFirebaseMsgService";
+
+
+    // Override onMessageReceived() method to extract the
         // title and
         // body from the message passed in FCM
         @Override
-        public void
-        onMessageReceived(RemoteMessage remoteMessage) {
+        public void onMessageReceived(RemoteMessage remoteMessage) {
             // First case when notifications are received via
             // data event
             // Here, 'title' and 'message' are the assumed names
@@ -39,32 +42,27 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
                 // Since the notification is received directly from
                 // FCM, the title and the body can be fetched
                 // directly as below.
-                showNotification(
-                        remoteMessage.getNotification().getTitle(),
-                        remoteMessage.getNotification().getBody());
+                showNotification( remoteMessage.getNotification().getTitle(),
+                                  remoteMessage.getNotification().getBody());
+
             }
         }
 
-        // Method to get the custom Design for the display of
-        // notification.
-//        private RemoteViews getCustomDesign(String title,
-//                                            String message) {
-//            RemoteViews remoteViews = new RemoteViews(
-//                    getApplicationContext().getPackageName(),
-//                    R.layout.notification);
-//            remoteViews.setTextViewText(R.id.title, title);
-//            remoteViews.setTextViewText(R.id.message, message);
-//            remoteViews.setImageViewResource(R.id.icon,
-//                    R.drawable.gfg);
-//            return remoteViews;
-//        }
+        // Method to get the custom Design for the display of notification.
+        private RemoteViews getCustomDesign(String title,
+                                            String message) {
+            RemoteViews remoteViews = new RemoteViews(
+                    getApplicationContext().getPackageName(), R.layout.notification);
+            remoteViews.setTextViewText(R.id.title, title);
+            remoteViews.setTextViewText(R.id.message, message);
+            remoteViews.setImageViewResource(R.id.icon, R.drawable.noti);
+            return remoteViews;
+        }
 
         // Method to display the notifications
-        public void showNotification(String title,
-                                     String message) {
+        public void showNotification(String title, String message) {
             // Pass the intent to switch to the MainActivity
-            Intent intent
-                    = new Intent(this, MainActivity.class);
+            Intent intent = new Intent(this, MainActivity.class);
             // Assign channel ID
             String channel_id = "notification_channel";
             // Here FLAG_ACTIVITY_CLEAR_TOP flag is set to clear
@@ -73,54 +71,44 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             // Pass the intent to PendingIntent to start the
             // next Activity
-            PendingIntent pendingIntent
-                    = PendingIntent.getActivity(
-                    this, 0, intent,
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
                     PendingIntent.FLAG_ONE_SHOT);
 
             // Create a Builder object using NotificationCompat
             // class. This will allow control over all the flags
-            NotificationCompat.Builder builder
-                    = new NotificationCompat
-                    .Builder(getApplicationContext(),
-                    channel_id)
-                    .setSmallIcon(R.drawable.gfg)
+            NotificationCompat.Builder builder = new NotificationCompat
+                    .Builder(getApplicationContext(), channel_id)
+                    .setSmallIcon(R.drawable.noti)
                     .setAutoCancel(true)
-                    .setVibrate(new long[]{1000, 1000, 1000,
-                            1000, 1000})
+                    .setVibrate(new long[]{1000, 1000, 1000, 1000, 1000})
                     .setOnlyAlertOnce(true)
                     .setContentIntent(pendingIntent);
 
             // A customized design for the notification can be
             // set only for Android versions 4.1 and above. Thus
             // condition for the same is checked here.
-//            if (Build.VERSION.SDK_INT
-//                    >= Build.VERSION_CODES.JELLY_BEAN) {
-//                builder = builder.setContent(
-//                        getCustomDesign(title, message));
-//            } // If Android Version is lower than Jelly Beans,
-//            // customized layout cannot be used and thus the
-//            // layout is set as follows
-//            else {
-//                builder = builder.setContentTitle(title)
-//                        .setContentText(message)
-//                        .setSmallIcon(R.drawable.gfg);
-//            }
+            if (Build.VERSION.SDK_INT
+                    >= Build.VERSION_CODES.JELLY_BEAN) {
+                builder = builder.setContent(
+                        getCustomDesign(title, message));
+            } // If Android Version is lower than Jelly Beans,
+            // customized layout cannot be used and thus the
+            // layout is set as follows
+            else {
+                builder = builder.setContentTitle(title)
+                        .setContentText(message)
+                        .setSmallIcon(R.drawable.noti);
+            }
             // Create an object of NotificationManager class to
             // notify the
             // user of events that happen in the background.
-            NotificationManager notificationManager
-                    = (NotificationManager) getSystemService(
-                    Context.NOTIFICATION_SERVICE);
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
             // Check if the Android Version is greater than Oreo
-            if (Build.VERSION.SDK_INT
-                    >= Build.VERSION_CODES.O) {
-                NotificationChannel notificationChannel
-                        = new NotificationChannel(
-                        channel_id, "web_app",
-                        NotificationManager.IMPORTANCE_HIGH);
-                notificationManager.createNotificationChannel(
-                        notificationChannel);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                NotificationChannel notificationChannel = new NotificationChannel(
+                        channel_id, "web_app", NotificationManager.IMPORTANCE_HIGH);
+                notificationManager.createNotificationChannel(notificationChannel);
             }
 
             notificationManager.notify(0, builder.build());
