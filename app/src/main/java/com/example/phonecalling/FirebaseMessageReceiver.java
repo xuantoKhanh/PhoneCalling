d6a72a83b9fc7b86205a405542fa5fca53e0d0e3
Admin <tokhanhxuan99@gmail.com>
2022-03-16 16:54:50 +0700

    demo on foreground

--- app/src/main/java/com/example/phonecalling/FirebaseMessageReceiver.java
+++ app/src/main/java/com/example/phonecalling/FirebaseMessageReceiver.java
@@ -24,14 +24,22 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
         @Override
         public void onMessageReceived(RemoteMessage remoteMessage) {
 
+            // attributes. Since here we do not have any data
+            // payload, This section is commented out. It is
+            // here only for reference purposes.
+        if(remoteMessage.getData().size()>0){
+            showNotification(remoteMessage.getData().get("title"),
+                          remoteMessage.getData().get("message"));
+        }
+
+
             // Second case when notification payload is
             // received.
             if (remoteMessage.getNotification() != null) {
                 // Since the notification is received directly from
                 // FCM, the title and the body can be fetched
                 // directly as below.
-                showNotification( remoteMessage.getNotification().getTitle(),
-                                  remoteMessage.getNotification().getBody());
+                showNotification( remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
 
             }
         }
--- app/src/main/java/com/example/phonecalling/MainActivity.java
+++ app/src/main/java/com/example/phonecalling/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get notification data info
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            //bundle must contain all info sent in "data" field of the notification
+        }
     }
 }
