9d722c97e89d53f2c313d72a116f939a966a0f10
Admin <tokhanhxuan99@gmail.com>
2022-03-16 17:58:56 +0700

    demo on foreground

--- app/src/main/java/com/example/phonecalling/FirebaseMessageReceiver.java
+++ app/src/main/java/com/example/phonecalling/FirebaseMessageReceiver.java
@@ -5,7 +5,9 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.RemoteViews;
 
@@ -55,6 +57,7 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
 
         // Method to display the notifications
         public void showNotification(String title, String message) {
+            Context context = getApplicationContext();
             // Pass the intent to switch to the MainActivity
             Intent intent = new Intent(this, ReceivingCall.class);
             // Assign channel ID
@@ -65,7 +68,8 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
             //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            startActivity(intent);
+
+            //startActivity(intent);
             // Pass the intent to PendingIntent to start the
             // next Activity
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
@@ -101,9 +105,9 @@ public class FirebaseMessageReceiver extends FirebaseMessagingService {
 
             // Check if the Android Version is greater than Oreo
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                NotificationChannel notificationChannel = new NotificationChannel(
-                        channel_id, "web_app", NotificationManager.IMPORTANCE_HIGH);
+                NotificationChannel notificationChannel = new NotificationChannel(channel_id, "web_app", NotificationManager.IMPORTANCE_HIGH);
                 notificationManager.createNotificationChannel(notificationChannel);
+                context.startForegroundService(intent);
             }
 
             notificationManager.notify(0, builder.build());
